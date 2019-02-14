b237e801c24f5437ddf5fcf02d1be2147528fa85
Polaris Broadcasting <kevin@polarisbroadcasting.com>
2019-02-14 03:32:26 -0500

    server loading and seeming to run

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
 cp ~/docker/nginx-rtmp/nginx.conf hostfiles
-sudo docker run -v ~/docker/nginx-rtmp/hostfiles:/hostfiles -p 8080:80 -p 1935:1935 -i -t --name nginx-rtmp --rm nginx_rtmp
+sudo docker run \
+	-i -t \
+	--name nginx-rtmp \
+	-v /home/kevin/apps/nginx-rtmp:/hostfiles \
+	--entrypoint=/bin/bash \
+	-p 8080:80 \
+	-p 1935:1935 \
+	--rm pmw1/nginx-rtmp
+
